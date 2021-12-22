22d9785c7444fdf02c1db2736d09bc62e5c4df12
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2021-12-22 14:43:21 +0100

    Update 404 view
    #3: Now with Bootstrap.

--- views/404.php
+++ views/404.php
@@ -1,19 +1,25 @@
+<?php
+
+use Fhooe\Router\Router;
+
+$basePath = Router::getBasePath();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>fhooe-router-skeleton: 404</title>
+    <title>fhooe/router-skeleton: 404</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="<?= $basePath ?>/../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= $basePath ?>/../vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
 </head>
 <body>
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-lg">
             <a class="navbar-brand" href="#">
-                <img src="../views/images/fhooe.svg" alt="" height="30" class="d-inline-block align-text-top">
-                fhooe-router-skeleton
+                <img src="<?= $basePath ?>/../views/images/fhooe.svg" alt="" height="30" class="d-inline-block align-text-top">
+                fhooe/router-skeleton
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,16 +28,16 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="">/</a>
+                        <a class="nav-link active" aria-current="page" href="<?= $basePath ?>">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="form">PHP Form</a>
+                        <a class="nav-link active" aria-current="page" href="<?= $basePath ?>/form">PHP Form</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="twigform">Twig Form</a>
+                        <a class="nav-link" href="<?= $basePath ?>/twigform">Twig Form</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="a/route/that/does/not/exist">Unknown Route (404)</a>
+                        <a class="nav-link" href="<?= $basePath ?>/a/route/that/does/not/exist">Unknown Route (404)</a>
                     </li>
                 </ul>
             </div>
@@ -40,28 +46,31 @@
 </header>
 <main>
     <div class="container-lg mt-lg-4">
-        <h1>fhooe-router-skeleton</h1>
-        <p class="lead">A skeleton application for the fhooe/router.</p>
+        <h1>fhooe/router-skeleton</h1>
+        <p class="lead">A skeleton application for fhooe/router.</p>
 
         <h2>404</h2>
         <p>This is the 404 view.</p>
-        <p>It is displayed, when a route is accessed that has not been defined in the router.</p>
+        <p>It is displayed when a route is accessed that has not been defined in the router.</p>
     </div>
 </main>
 
 <div class="container-lg">
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div class="col-lg-9 d-flex align-items-center">
-            <img src="images/fhooe.svg" alt="" height="24" class="me-2">
+            <img src="<?= $basePath ?>/../views/images/fhooe.svg" alt="" height="24" class="me-2">
             <span class="text-muted">© FH Oberösterreich | Department of Digital Media</span>
         </div>
 
         <ul class="nav col-lg-3 justify-content-end list-unstyled d-flex">
-            <li class="ms-3"><a class="text-muted" href="https://github.com/Digital-Media/fhooe-router-skeleton"><i
-                    class="bi-github"></i></a></li>
+            <li class="ms-3">
+                <a class="text-muted" href="https://github.com/Digital-Media/fhooe-router-skeleton">
+                    <i class="bi-github"></i>
+                </a>
+            </li>
         </ul>
     </footer>
 </div>
-<script src="../vendor/twbs/bootstrap/dist/js/bootstrap.js"></script>
+<script src="<?= $basePath ?>/../vendor/twbs/bootstrap/dist/js/bootstrap.js"></script>
 </body>
 </html>
