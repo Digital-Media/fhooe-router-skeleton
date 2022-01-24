50e6b00bc00ba73420229d560f75e0a8533c8db4
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2022-01-24 01:22:06 +0100

    Remove debugging settings for PHP
    This should be enabled on your (development) server.

--- public/index.php
+++ public/index.php
@@ -18,20 +18,6 @@ require "../vendor/autoload.php";
 //session_start();
 
 /**
- * Turn on debugging output to get more useful error messages while developing.
- */
-const DEBUG = false;
-if (DEBUG) {
-    echo "<br>WARNING: Debugging is enabled. Set DEBUG to false for production use in " . __FILE__;
-    echo "<br>Connect via SSH and send tail -f /var/log/apache2/error.log";
-    echo " to see errors not displayed in Browser<br><br>";
-    error_reporting(E_ALL);
-    ini_set("html_errors", "1");
-    ini_set("display_errors", "1");
-    ini_set("display_startup_errors", "1");
-}
-
-/**
  * Instantiated Router invocation. Create an object, define the routes and run it.
  */
 // Create a new Router object.
